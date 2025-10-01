f4feabc77b920d7d3203b1ea824d1a854e9fc3f7
Kyugito <113055222+Kyugito666@users.noreply.github.com>
2025-10-01 14:32:28 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Skrip instalasi untuk pengguna Linux & macOS
+# GitHub: Kyugito666
+
+echo "======================================================="
+echo "    Installer untuk PowerShell Auto-Tab Manager"
+echo "        by Kyugito666"
+echo "======================================================="
+echo ""
+echo "Script ini akan menginstal semua package Python yang dibutuhkan."
+echo ""
+
+echo "[+] Memulai instalasi dari requirements.txt..."
+python3 -m pip install -r requirements.txt
+
+echo ""
+echo "[+] Instalasi Selesai!"
+echo "Anda sekarang bisa menjalankan program dengan 'python3 main.py'."
+echo ""
